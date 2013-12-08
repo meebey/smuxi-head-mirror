901aa83c42dc2e056e75257116a115b5c25bc0d0
Mirco Bauer <meebey@meebey.net>
2013-12-08 18:00:23 +0100

    Update from HEAD: 9f55cf1 Debian: install APT config file to the right place

--- src/Frontend-GNOME/Preferences/PreferencesDialog.cs
+++ src/Frontend-GNOME/Preferences/PreferencesDialog.cs
@@ -356,7 +356,15 @@ namespace Smuxi.Frontend.Gnome
                     if (enc.EncodingName.Contains(" (")) {
                         encodingName = encodingName.Substring(0, enc.EncodingName.IndexOf(" ("));
                     }
-                    store.AppendValues(enc.BodyName.ToUpper() + " - " + encodingName, enc.BodyName.ToUpper());
+                    var normalizedBodyName = enc.BodyName.ToUpper();
+                    if (normalizedBodyName.StartsWith("KOI8")) {
+                        // this is the only non-ISO encoding that doesn't use
+                        // WINDOWS as prefix and confuses users
+                        normalizedBodyName = String.Format("WINDOWS-{0} / {1}",
+                                                           enc.CodePage,
+                                                           normalizedBodyName);
+                    }
+                    store.AppendValues(normalizedBodyName + " - " + encodingName, enc.BodyName.ToUpper());
                 } catch (NotSupportedException) {
                 }
             }
