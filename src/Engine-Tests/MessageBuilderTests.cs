9a8b056ef84c38d866174314db55be40d7c94c19
Mirco Bauer <meebey@meebey.net>
2014-01-15 06:01:23 +0100

    Update from HEAD: ed9d23b Engine(-Tests): fixed matching URLs that contain a ! in the path

--- src/Engine-Tests/MessageBuilderTests.cs
+++ src/Engine-Tests/MessageBuilderTests.cs
@@ -513,6 +513,20 @@ namespace Smuxi.Engine
             builder.TimeStamp = DateTime.MinValue;
             builder.Append(new UrlMessagePartModel("http://www.stack.nl/~jilles/cgi-bin/hgwebdir.cgi/charybdis/raw-rev/9d769851c1c7"));
             TestMessage(msg, builder.ToMessage());
+
+            msg = "<RAOF> meebey: Associated mono branch is master-experimental in git+ssh://git.debian.org/~/public_git/mono.git";
+            builder = new MessageBuilder();
+            builder.TimeStamp = DateTime.MinValue;
+            builder.Append(new TextMessagePartModel("<RAOF> meebey: Associated mono branch is master-experimental in "));
+            builder.Append(new UrlMessagePartModel("git+ssh://git.debian.org/~/public_git/mono.git"));
+            TestMessage(msg, builder.ToMessage());
+
+            msg = "<knocte> meebey: does this URL highlight ok with latest master?  https://groups.google.com/forum/#!topic/fsharp-opensource/KLejo_vw5R4";
+            builder = new MessageBuilder();
+            builder.TimeStamp = DateTime.MinValue;
+            builder.Append(new TextMessagePartModel("<knocte> meebey: does this URL highlight ok with latest master?  "));
+            builder.Append(new UrlMessagePartModel("https://groups.google.com/forum/#!topic/fsharp-opensource/KLejo_vw5R4"));
+            TestMessage(msg, builder.ToMessage());
         }
 
         [Test]
